64d4a09faa82707969b3238ea60f75b2f40a3836
jaidenmuruvan <2726725@students.wits.ac.za>
2025-10-18 02:11:34 +0200

    Trial starter done

--- Assets/Scripts/TimeTrigger.cs
+++ Assets/Scripts/TimeTrigger.cs
@@ -1,18 +1,59 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.InputSystem; 
 
-public class TimeTrigger : MonoBehaviour
+public class TrialTrigger : MonoBehaviour
 {
-    public GameObject timerManager;
-    public Text timerText;
+    public GameObject promptUI;        
+    public GameObject timerObject;
+    public GameObject trialOneEnemies;
+
+    private bool playerInRange = false;
+
+    private void Start()
+    {
+        if (promptUI != null)
+            promptUI.SetActive(false);
+
+        if (timerObject != null)
+            timerObject.SetActive(false); 
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            timerManager.SetActive(true);
-            timerText.gameObject.SetActive(true);
+            playerInRange = true;
+            if (promptUI != null)
+                promptUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            if (promptUI != null)
+                promptUI.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInRange && Keyboard.current.eKey.wasPressedThisFrame) //New Input System check
+        {
+            if (timerObject != null)
+            {
+                timerObject.SetActive(true);
+                trialOneEnemies.SetActive(true);
+            }
+                 
+
+            if (promptUI != null)
+                promptUI.SetActive(false);
+
             
+            gameObject.SetActive(false);
         }
     }
 }

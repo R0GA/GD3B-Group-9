ea9f2cdd4686af162edc0d076106c3cebf36412d
jaidenmuruvan <2726725@students.wits.ac.za>
2025-11-12 11:53:53 +0200

    Commit

--- Assets/Scripts/CrystalTrigger.cs
+++ Assets/Scripts/CrystalTrigger.cs
@@ -6,16 +6,26 @@ public class CrystalTrigger : MonoBehaviour
     public GameObject singleCrystal;
     public GameObject multipleCrystals;
 
+    private AudioSource crystalAudio;
+
+    private void Start()
+    {
+        crystalAudio.GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         StartCoroutine(CrystalFall());
     }
     private IEnumerator CrystalFall()
     {
+        crystalAudio.Play();
         singleCrystal.SetActive(true);
 
         yield return new WaitForSeconds(2f);
 
         multipleCrystals.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+        crystalAudio.Stop();
     }
 }
